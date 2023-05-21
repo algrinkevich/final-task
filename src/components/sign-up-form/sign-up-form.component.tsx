8df6cedf7443643576ec87b4c50acfae17858fa6
agrinkevich <grinkevich.alina@gmail.com>
2023-05-21 19:13:55 +0600

    Add sign up form with validation

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -1,51 +1,92 @@
-import Button, { ButtonType } from "../../components/button/button.component"
-import FormInput from "../../components/form-input/form-input.component"
+import { Form, Formik } from "formik";
 
-import "./sign-up-form.styles.scss"
+import Button, { ButtonType } from "../../components/button/button.component";
+import FormInput from "../../components/form-input/form-input.component";
+
+import "./sign-up-form.styles.scss";
 
 const SignUpForm = () => {
   return (
     <div className="sign-up-container">
       <h2 className="form-title">{"Sign up"}</h2>
-      <FormInput
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Enter your email"
-        title="Email"
-        styleClasses="sign-up-container__input"
-        required
-      />
-      <FormInput
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Enter your password"
-        title="Password"
-        styleClasses="sign-up-container__input"
-        required
-      />
-      <FormInput
-        type="password"
-        id="confirm-password"
-        name="confirm-password"
-        placeholder="Enter your password again"
-        title="Repeat Password"
-        styleClasses="sign-up-container__input"
-        required
-      />
-      <Button
-        buttonType={ButtonType.INITIAL_BLUE}
-        styleClasses="sign-up-container__button"
+      <Formik
+        initialValues={{ email: "", password: "", "confirm-password": "" }}
+        onSubmit={() => alert("Hiiii!")}
       >
-        {"Create Account"}
-      </Button>
+        {({ errors, touched, values }) => {
+          const showEmailError = errors.email && touched.email;
+          const showPasswordError = errors.password && touched.password;
+
+          const showConfirmPasswordError =
+            errors["confirm-password"] && touched["confirm-password"];
+
+          return (
+            <Form>
+              <div className="sign-up-container__input">
+                <FormInput
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  title="Email"
+                  styleClasses={`${showEmailError ? "error" : ""}`}
+                />
+                {showEmailError && (
+                  <div className="error-message">{errors.email}</div>
+                )}
+              </div>
+
+              <div className="sign-up-container__input">
+                <FormInput
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter your password"
+                  title="Password"
+                  styleClasses={`${showPasswordError ? "error" : ""}`}
+                />
+                {showPasswordError && (
+                  <div className="error-message">{errors.password}</div>
+                )}
+              </div>
+              <div className="sign-up-container__input">
+                <FormInput
+                  type="password"
+                  id="confirm-password"
+                  name="confirm-password"
+                  placeholder="Enter your password again"
+                  title="Repeat Password"
+                  styleClasses={`${showConfirmPasswordError ? "error" : ""}`}
+                  validate={(value) =>
+                    value !== values.password
+                      ? "Passwords do not match"
+                      : undefined
+                  }
+                />
+                {showConfirmPasswordError && (
+                  <div className="error-message">
+                    {errors["confirm-password"]}
+                  </div>
+                )}
+              </div>
+
+              <Button
+                buttonType={ButtonType.INITIAL_BLUE}
+                styleClasses="sign-up-container__button"
+              >
+                {"Create Account"}
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
+
       <span className="sign-up-footer">
         {"Already have an account? "}
         <strong>{"Login"}</strong>
       </span>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
