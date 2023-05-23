35f5d7981e1394df0adcb0e09163433714a9c121
agrinkevich <grinkevich.alina@gmail.com>
2023-05-23 17:19:56 +0600

    Implement Search page without results

--- src/pages/search-page/search-page.component.tsx
+++ src/pages/search-page/search-page.component.tsx
@@ -1,9 +1,28 @@
+import { ReactComponent as FiltersIcon } from "../../assets/filters.svg";
+import Button, { ButtonType } from "../../components/button/button.component";
+
 import "./search-page.style.scss";
 
 const SearchPage = () => {
   return (
-    <div>
-      <span>{"Search Page"}</span>
+    <div className="search-page-container">
+      <div className="search-page-header">
+        <input
+          type="search"
+          className="input-search"
+          placeholder="Enter search value"
+        />
+        <Button buttonType={ButtonType.BASE} styleClasses="search-btn">
+          {"Search"}
+        </Button>
+        <Button buttonType={ButtonType.FILTERS}>
+          <FiltersIcon />
+        </Button>
+      </div>
+      <p className="no-data-placeholder">
+        {"No data to display"}
+        <br /> {"Please start search to display results"}
+      </p>
     </div>
   );
 };
